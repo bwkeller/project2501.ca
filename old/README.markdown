4d0b0d702fa08fe07c138442bb33f990fc1a790b
Ben Keller <malzraa@gmail.com>
2013-03-27 20:50:13 -0400

    Removed the extraneous hyde stuff.

--- old/README.markdown
+++
@@ -1,3 +0,0 @@
-# My Personal Webpage
-Use publish.sh to compile the website with hyde and publish it to my
-McMaster webspace.
--- old/publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-hyde gen
-hyde publish -c prod.yaml
-scp ~/www_publish.zip kellerbw@physwww.mcmaster.ca:~
-ssh kellerbw@physwww.mcmaster.ca "unzip -o ~/www_publish.zip -d ~/public_html"
