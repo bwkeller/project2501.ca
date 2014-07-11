9ec6132536726bda299b45de2c24dc915cd0beb0
Ben Keller <malzraa@gmail.com>
2014-07-10 20:07:11 -0400

    Added deploy commands

--- conf.py
+++ conf.py
@@ -278,7 +278,7 @@ REDIRECTIONS = []
 # And then do a backup, or run `nikola ping` from the `ping`
 # plugin (`nikola install_plugin ping`).
 # To do manual deployment, set it to []
-# DEPLOY_COMMANDS = []
+DEPLOY_COMMANDS = ['rsync -rav output/* kellerbw@physwww.physics.mcmaster.ca:~/public_html']
 
 # For user.github.io/organization.github.io pages, the DEPLOY branch
 # MUST be 'master', and 'gh-pages' for other repositories.
