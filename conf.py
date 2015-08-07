e75d2337761ca81e31740adcf0b23730219affe7
Ben Keller <malzraa@gmail.com>
2015-08-07 12:16:09 -0400

    Use teasers

--- conf.py
+++ conf.py
@@ -409,7 +409,7 @@ IMAGE_FOLDERS = {'images':'assets/img/'}
 # }
 
 # Show only teasers in the index pages? Defaults to False.
-# INDEX_TEASERS = False
+INDEX_TEASERS = True
 
 # HTML fragments with the Read more... links.
 # The following tags exist and are replaced for you:
