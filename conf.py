f61c3412b7351331af4d41904963ee49ec5b5822
Ben Keller <malzraa@gmail.com>
2015-08-08 12:51:10 -0400

    Fixed deploy command

--- conf.py
+++ conf.py
@@ -288,7 +288,7 @@ REDIRECTIONS = []
 # And then do a backup, or run `nikola ping` from the `ping`
 # plugin (`nikola install_plugin ping`).
 # To do manual deployment, set it to []
-DEPLOY_COMMANDS = {'default':'rsync -rav output/* kellerbw@physserv.physics.mcmaster.ca:/net/physwww/1/home/kellerbw/public_html'}
+DEPLOY_COMMANDS = {'default':['rsync -rav output/* kellerbw@physserv.physics.mcmaster.ca:/net/physwww/1/home/kellerbw/public_html']}
 
 # For user.github.io/organization.github.io pages, the DEPLOY branch
 # MUST be 'master', and 'gh-pages' for other repositories.
